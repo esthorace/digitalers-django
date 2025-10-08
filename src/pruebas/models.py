0f296db91a97fe2a2f27a620c9f4a9c39b784720
Esteban <esteban.horacio.acevedo@gmail.com>
2025-10-07 22:06:50 -0300

    Crea el modelo Servicio y registra en admin

--- src/pruebas/models.py
+++ src/pruebas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.IntegerField(default=0)
+    fecha = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.nombre
